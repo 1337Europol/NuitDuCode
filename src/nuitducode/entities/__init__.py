3656e228dfa45e186bca71928e404639898bfb05
Europol <290176053+1337Europol@users.noreply.github.com>
2026-06-12 16:53:18 +0200

    Update __init__.py

--- src/nuitducode/entities/__init__.py
+++ src/nuitducode/entities/__init__.py
@@ -4,7 +4,7 @@ from .ennemi import Ennemi
 from .joueur import Joueur
 from .projectile import Projectile
 from .powerup import Powerup
-from .etoile import Etoile
+from .etoile import Etoile, ChampEtoiles
 from .explosion import Explosion
 
 __all__ = ["Ennemi", "Joueur", "Projectile", "Powerup", "Etoile", "Explosion"]
